96627e61df3ccb2dbc87c5d7c2618cab37a79db2
Muad-Ahmed <muad8010@gmail.com>
2025-08-23 13:05:57 +0300

    init: install vite, mui,react-router and configuration setup for the project

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
