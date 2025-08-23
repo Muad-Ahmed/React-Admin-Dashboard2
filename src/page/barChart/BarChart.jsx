aa43b3bb15d092d06f6e0e2746a1bb0fed3affcd
Muad-Ahmed <muad8010@gmail.com>
2025-08-23 17:21:16 +0300

    feat: add BarChart page with bar.jsx component, install @nivo/bar

---
+++ src/page/barChart/BarChart.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import { Box} from "@mui/material";
+import Bar from "./bar";
+
+const BarChart = () => {
+  return (
+    <Box>
+
+      <Bar />
+    </Box>
+  );
+};
+
+export default BarChart;
