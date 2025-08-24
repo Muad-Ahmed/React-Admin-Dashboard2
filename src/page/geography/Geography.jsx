982f8060bfff7ee20be6810596f34077cab77738
Muad-Ahmed <muad8010@gmail.com>
2025-08-24 11:20:39 +0300

    add main geography page

---
+++ src/page/geography/Geography.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Geography = () => {
+  return (
+    <Box>
+      <Header title="Geography" subTitle="Simple Geography Chart" />
+
+   
+    </Box>
+  );
+};
+
+export default Geography;
