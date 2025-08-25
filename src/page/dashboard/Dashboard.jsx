34132756682571e17dfb0fccb41d8f7c42d3261c
Muad-Ahmed <muad8010@gmail.com>
2025-08-25 11:21:39 +0300

    feat: add third row to dashboard page, contains: pie chart, bar chart and geography chart

--- src/page/dashboard/Dashboard.jsx
+++ src/page/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import Row1 from "./Row1";
 import Row2 from "./Row2";
+import Row3 from "./Row3";
 import Button from "@mui/material/Button";
 import { DownloadOutlined } from "@mui/icons-material";
 import { Box, Stack, Typography } from "@mui/material";
@@ -33,6 +34,7 @@ const Dashboard = () => {
       </Stack>
       <Row1 />
       <Row2 />
+      <Row3 />
     </div>
   );
 };
